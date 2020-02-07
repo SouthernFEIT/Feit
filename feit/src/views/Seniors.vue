c971a9915a554156b6776e9c67df4687715fb9b3
limgwei <40509569+limgwei@users.noreply.github.com>
2020-02-08 00:38:56 +0800

    'seniors'

--- feit/src/views/Seniors.vue
+++ feit/src/views/Seniors.vue
@@ -1,11 +1,354 @@
 <template>
-  <h1>Seniors</h1>
+  <div>
+    <div class="top">
+      <h1 class>Seniors in Work</h1>
+      <br />
+      <p>Finds out what company the FEIT graduated students have been worked with.</p>
+    </div>
+    <!-- top -->
+    <!-- content -->
+    <div class="center">
+      <div style="text-align: center;margin-bottom:2%">Board</div>
+      <div class="cards">
+        <div v-for="(senior,index) in seniors" v-bind:key="index">
+          <div v-if="index >= indexe && index < indexes">
+            <v-card max-width="300" class="cardInside" max-height="300">
+              <v-list-item class="listItem" three-line>
+                <v-list-item-avatar color="grey">
+                  <v-img v-bind:src="senior.imgUrl" height="194"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title style="margin-bottom:3%">
+                    <b>{{senior.name}}</b>
+                  </v-list-item-title>
+                  <v-list-item-subtitle style="font-size:12px;padding:0;">{{senior.position}}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-card-actions>
+                <v-list-item-content class="greyColor">
+                  <v-list-item-title class="headline listActions">
+                    <v-btn text color="deep-purple accent-4">
+                      <v-avatar size="36" color="#F1EEF9">
+                        <img src="../../svg/location.svg" style="width:18px" />
+                      </v-avatar>
+                      <span class="black--text">{{senior.location}}</span>
+                    </v-btn>
+                  </v-list-item-title>
+                  <v-list-item-subtitle class="listActions">
+                    <v-btn text color="deep-purple accent-4">
+                      <v-avatar size="36" color="#F1EEF9">
+                        <img src="../../svg/phone.svg" style="width:18px" />
+                      </v-avatar>
+                      <span class="black--text">{{senior.phoneNumber}}</span>
+                    </v-btn>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-card-actions>
+            </v-card>
+          </div>
+        </div>
+        <!-- v-if -->
+      </div>
+      <!-- cards -->
+      <div class="text-center">
+        <v-pagination v-model="page" :length="Math.ceil(seniors.length/9)"></v-pagination>
+      </div>
+    </div>
+    <!-- center -->
+    <div class="seniorThoughts">
+      <h1 class="wordSeniorThoughts">Senior Thought</h1>
+      <div class="cards space">
+        <div class="cardsForSenior">
+          <v-card
+            max-width="500"
+            min-width="500"
+            class="cardInside"
+            v-for="index in 2"
+            v-bind:key="index"
+          >
+            <v-list-item>
+              <v-list-item-avatar color="grey" height="90" width="90">
+                <v-img v-bind:src="thoughts[index-1].imgUrl"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <div class="overline mb-4">{{thoughts[index-1].overline}}</div>
+                <v-list-item-title style="margin-bottom:3%">
+                  <b>{{thoughts[index-1].name}}</b>
+                </v-list-item-title>
+                <v-list-item-subtitle
+                  style="font-size:12px;padding:0;"
+                >{{thoughts[index-1].position}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-card-actions>
+              <v-list-item-content class="greyColor">
+                <v-list-item-subtitle>
+                  <div class="text">{{thoughts[index-1].text}}</div>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-card-actions>
+          </v-card>
+        </div>
+        <!-- left -->
+        <div class="right">
+          <v-card max-width="500" min-width="500" class="cardInside">
+            <v-list-item>
+              <v-list-item-avatar color="grey" height="90" width="90">
+                <v-img v-bind:src="thoughts[2].imgUrl"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <div class="overline mb-4">{{thoughts[2].overline}}</div>
+                <v-list-item-title style="margin-bottom:3%">
+                  <b>{{thoughts[2].name}}</b>
+                </v-list-item-title>
+                <v-list-item-subtitle style="font-size:12px;padding:0;">{{thoughts[2].position}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-card-actions>
+              <v-list-item-content class="greyColor">
+                <v-list-item-subtitle>{{thoughts[2].text}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-card-actions>
+          </v-card>
+
+          <v-card max-width="500" min-width="500" class="cardInside">
+            <v-list-item>
+              <v-list-item-avatar color="grey" height="90" width="90">
+                <v-img v-bind:src="thoughts[3].imgUrl"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <div class="overline mb-4">{{thoughts[3].overline}}</div>
+                <v-list-item-title style="margin-bottom:3%">
+                  <b>{{thoughts[3].name}}</b>
+                </v-list-item-title>
+                <v-list-item-subtitle style="font-size:12px;padding:0;">{{thoughts[3].position}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-card-actions>
+              <v-list-item-content class="greyColor">
+                <v-list-item-subtitle>{{thoughts[3].text}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-card-actions>
+          </v-card>
+        </div>
+        <!-- right -->
+      </div>
+      <!-- cards -->
+    </div>
+    <!-- seniorThoughts -->
+  </div>
 </template>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script>
-  export default {
-    name: "Seniors"
-  };
+export default {
+  name: "Seniors",
+  data() {
+    return {
+      page: 1,
+      indexes: 9,
+      indexe: 0,
+      seniors: [
+        {
+          name: "John Doe",
+          position: "Junior Front-end Developer ",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Doe",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Doe",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          name: "John Does",
+          position: "Junior Front-end Developer",
+          location: "Johor",
+          phoneNumber: "012-3456789",
+          imgUrl: require("../assets/images/hero.png")
+        }
+      ], //seniors
+      thoughts: [
+        {
+          overline: "Ex-boss",
+          name: "John Doe",
+          position: "Junior",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+            "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          overline: "Ex-boss",
+          name: "John Doe",
+          position: "Junior",
+          text: "Lorem impusum",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          overline: "Ex-boss",
+          name: "John Doe",
+          position: "Junior",
+          text: "Lorem impusum",
+          imgUrl: require("../assets/images/hero.png")
+        },
+        {
+          overline: "Ex-boss",
+          name: "John Doe",
+          position: "Junior",
+          text: "Lorem impusum",
+          imgUrl: require("../assets/images/hero.png")
+        }
+      ]
+    }; //return
+  }, //data
+  watch: {
+    page: function(val) {
+      this.indexes = val * 9;
+      this.indexe = this.indexes - 9;
+    }
+  } //watch
+};
 </script>
 <style scoped>
-
+.top {
+  background-image: url("../assets/images/senior_in_work_hero.png");
+  background-position: center top;
+  background-size: 100%;
+  color: white !important;
+  padding: 10% 30% 10% 30%;
+  text-align: center;
+}
+.center {
+  padding: 11%;
+  background: #f9f9fd;
+}
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  white-space: nowrap;
+}
+.cardInside {
+  margin: 5% 0% 5% 5%;
+}
+.greyColor {
+  background: #f9f9fd;
+  margin: 0;
+}
+.v-card__actions {
+  padding: 0;
+}
+.seniorThoughts {
+  background: #4d17e3;
+  color: white;
+  padding: 5%;
+}
+.listItem {
+  padding: 7%;
+}
+.listActions {
+  padding: 4%;
+}
+.space {
+  justify-content: space-evenly;
+}
+.v-list-item__subtitle {
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  white-space: unset;
+  padding: 5%;
+}
+.right {
+  margin-top: 5%;
+}
+.wordSeniorThoughts {
+  padding: 5%;
+}
 </style>
